4ee3271377655e3337e236d3069916c8a08ab1b1
faisal <faisalhossain.dev@gmail.com>
2026-02-02 21:57:54 +0600

    login from

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import LoginFrom from "./LoginForm";
+// import App from "./App";
 
 // import{ Button} from "./Button";
 // const mainButton = "Press Here";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
     {/* <Button name={mainButton} abs="blue"/> */}
+    <LoginFrom />
   </StrictMode>,
 );
